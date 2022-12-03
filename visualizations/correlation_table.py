d9a4d024a89546b18e39e8eef4f7db5a7cac9a51
Anna <anna.martirosyan@powerdata.de>
2022-12-03 19:42:54 +0400

    correlation table

--- visualizations/correlation_table.py
+++ visualizations/correlation_table.py
@@ -5,25 +5,24 @@ import matplotlib.pyplot as plt
 
 adv_stats = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players2017/Adv_2017wrangled.csv')
 adv_stats.drop(adv_stats.columns[[0, 1, 20, 25, 3, 4, 5, 6, 7, 30]], axis = 1, inplace = True)
-print(adv_stats.columns)
-
+# print(adv_stats.columns)
 
 perGame = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players2017/PerGame_2017wrangled.csv')
-perGame.drop(perGame.columns[[0, 1, 20, 25]], axis = 1, inplace = True)
-print(perGame.columns)
+perGame.drop(perGame.columns[[0, 1, 20, 25]], axis=1, inplace=True)
+# print(perGame.columns)
 
-perGameAdv = perGame.merge(adv_stats, on = 'Player', how = 'inner')
-print(perGameAdv.columns)
+perGameAdv = perGame.merge(adv_stats, on='Player', how='inner')
+# print(perGameAdv.columns)
 
 fa = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/salary_data/fixed_fa_signings/2017.csv')
-fa.drop(fa.columns[[0, 1, 2, 3, 4, 5, 6]], axis = 1, inplace = True)
-print(fa.columns)
+fa.drop(fa.columns[[0, 1, 2, 3, 4, 5, 6]], axis=1, inplace=True)
+# print(fa.columns)
 
 final = perGameAdv.merge(fa, left_on = 'fix', right_on = 'name', how = 'inner')
-# drop 'fix' and 'name
+# drop 'fix' and 'name'
 final.drop(final.columns[[27]], axis = 1, inplace = True)
 final.head()
-final2 = final.copy(deep = True)
+final2 = final.copy(deep=True)
 
 final.drop(final.columns[[0, 1, 3, 49]], axis = 1, inplace = True)
 
@@ -34,19 +33,28 @@ plt.figure(figsize=(30,30))
 
 plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
 
+#make all values floating points, compute pairwise correlation of columns,
+# excluding NA/null values
+
 corr = final.astype(float).corr().round(2)
-# corr = smalls.astype(float).corr().round(2)
 mask = np.zeros_like(corr, dtype=bool)
 
 # mask upper diagonal of heatmap
 mask[np.triu_indices_from(mask)] = True
 
-fig1 = sns.heatmap(corr, mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
+fig1 = sns.heatmap(corr,
+                   mask=mask,
+                   linewidths=0.1,
+                   vmax=1.0,
+                   square=True,
+                   cmap=colormap,
+                   linecolor='white',
+                   annot=True)
+
 fig1.figure.savefig("correlation_table.png")
 
-#######################################################################################################################
+########################################################################################################################
 
-print(final2.columns)
 #Selecting candidates
 final2.drop(final2.columns[[0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21,
                          25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
@@ -61,6 +69,7 @@ plt.figure(figsize=(30,30))
 plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
 
 corr = final2.astype(float).corr().round(2)
+
 mask = np.zeros_like(corr, dtype=bool)
 
 # mask upper diagonal of heatmap
@@ -69,11 +78,10 @@ mask[np.triu_indices_from(mask)] = True
 fig2 = sns.heatmap(corr,mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
 fig2.figure.savefig("more_precise_correlation_table.png")
 
-#######################################################################################################################
+########################################################################################################################
 
 #Selecting candidates
 final2.drop(final2.columns[[0, 1, 2, 3, 5, 6, 9, 10, 11, 12, 13, 15 ]], axis = 1, inplace = True)
-print(final2.columns)
 
 # Check correlation between features
 sns.set(font_scale=3)
@@ -91,3 +99,16 @@ mask[np.triu_indices_from(mask)] = True
 fig3 = sns.heatmap(corr, mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap,
             linecolor='white', annot=True, annot_kws={"size": 50})
 fig3.figure.savefig("final_correlation_table.png")
+
+
+# In baseball, value over replacement player (or VORP) is a statistic popularized by Keith Woolner that
+# demonstrates how much a hitter or pitcher contributes to their team in comparison to a replacement-level
+# player who is an average fielder at that position and a below average hitter.
+
+
+# Average points per game
+
+# Average turnovers per game
+
+# Defensive rebounds per game
+
