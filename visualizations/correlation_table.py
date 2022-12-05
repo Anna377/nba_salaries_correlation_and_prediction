44a2093d67b875bb16f02e88fd65e5f631581bb5
Anna <anna.martirosyan@powerdata.de>
2022-12-06 00:39:59 +0400

    adjustments

--- visualizations/correlation_table.py
+++ visualizations/correlation_table.py
@@ -3,70 +3,71 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-adv_stats = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players2017/Adv_2017wrangled.csv')
-adv_stats.drop(adv_stats.columns[[0, 1, 20, 25, 3, 4, 5, 6, 7, 30]], axis = 1, inplace = True)
-print(adv_stats.columns)
-
-
 adv_stats = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players2022/Adv_2022wrangled.csv')
-adv_stats.drop(adv_stats.columns[[0, 1, 20, 25, 3, 4, 5, 6, 7, 30]], axis = 1, inplace = True)
-print(adv_stats.columns)
+adv_stats.drop(adv_stats.columns[[0, 1,  3, 4, 5, 18, 23,28,29]], axis = 1, inplace = True)
 
-#
-# perGame = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players22/PerGame_2022wrangled.csv')
-# perGame.drop(perGame.columns[[0, 1, 20, 25]], axis=1, inplace=True)
-# # print(perGame.columns)
-#
-# perGameAdv = perGame.merge(adv_stats, on='Player', how='inner')
-# # print(perGameAdv.columns)
-#
-# fa = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/salary_data/fixed_fa_signings/2022.csv')
-# fa.drop(fa.columns[[0, 1, 2, 3, 4, 5, 6]], axis=1, inplace=True)
-# # print(fa.columns)
-#
-# final = perGameAdv.merge(fa, left_on = 'fix', right_on = 'name', how = 'inner')
-# # drop 'fix' and 'name'
-# final.drop(final.columns[[27]], axis = 1, inplace = True)
-# final.head()
-# final2 = final.copy(deep=True)
-#
-# final.drop(final.columns[[0, 1, 3, 49]], axis = 1, inplace = True)
-#
-# #Check correlation between features
-# colormap = plt.cm.magma
-# sns.set(font_scale=1)
-# plt.figure(figsize=(30,30))
-#
-# plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
-#
-# #make all values floating points, compute pairwise correlation of columns,
-# # excluding NA/null values
-#
-# corr = final.astype(float).corr().round(2)
-# mask = np.zeros_like(corr, dtype=bool)
-#
-# # mask upper diagonal of heatmap
-# mask[np.triu_indices_from(mask)] = True
-#
-# fig1 = sns.heatmap(corr,
-#                    mask=mask,
-#                    linewidths=0.1,
-#                    vmax=1.0,
-#                    square=True,
-#                    cmap=colormap,
-#                    linecolor='white',
-#                    annot=True)
-#
-# fig1.figure.savefig("correlation_table.png")
-#
-# ########################################################################################################################
-#
-# #Selecting candidates
-# final2.drop(final2.columns[[0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21,
-#                          25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-#                          41, 42, 49]], axis = 1, inplace = True)
+
+perGame = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/player_data_wrangled/players2022/PerGame_2022wrangled.csv')
+perGame.drop(perGame.columns[[0, 1, 18, 23]], axis=1, inplace=True)
+
+perGameAdv = perGame.merge(adv_stats, on='Player', how='inner')
+
+fa = pd.read_csv('/Users/annamartirosyan/PycharmProjects/NBA_salaries/data/salary_data/fixed_fa_signings/2022.csv')
+fa.drop(fa.columns[[0,2,3,4,5,6,7,10]], axis=1, inplace=True)
+
+
+final = perGameAdv.merge(fa, left_on = 'fix', right_on = 'name', how = 'inner')
+# drop 'fix' and 'name'
+final.drop(final.columns[[27]], axis = 1, inplace = True)
+final.head()
+final2 = final.copy(deep=True)
+print(final.columns)
+final.drop(final.columns[[0, 26,25, 46]], axis = 1, inplace = True)
+
+# Check correlation between features
+colormap = plt.cm.magma
+sns.set(font_scale=1)
+plt.figure(figsize=(30,30))
+
+plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
+
+#make all values floating points, compute pairwise correlation of columns,
+# excluding NA/null values
+
+corr = final.astype(float).corr().round(2)
+mask = np.zeros_like(corr, dtype=bool)
+
+# mask upper diagonal of heatmap
+mask[np.triu_indices_from(mask)] = True
+
+fig1 = sns.heatmap(corr,
+                   mask=mask,
+                   linewidths=0.1,
+                   vmax=1.0,
+                   square=True,
+                   cmap=colormap,
+                   linecolor='white',
+                   annot=True)
+
+fig1.figure.savefig("correlation_table.png")
+
+########################################################################################################################
+print(final2.columns)
+#Selecting candidates
+final2.drop(final2.columns[[0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21,
+                         25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                         41, 42]], axis = 1, inplace = True)
 # print(final2.columns)
 #
+
+#
+# Index(['MP', 'FG', 'FGA', 'FT', 'DRB', 'STL', 'BLK', 'TOV', 'PS.G', 'PER',
+#        'DWS', 'OBPM', 'DBPM', 'BPM', 'VORP', 'total-contract', 'average'],
+#       dtype='object')
+ #piti mnan sranq
+
+
+
 # #Check correlation between features
 # sns.set(font_scale=2)
 # colormap = plt.cm.magma
