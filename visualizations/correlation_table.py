bc9b1ad89ba3439837d51f300b36494248ebe7e4
Anna <anna.martirosyan@powerdata.de>
2022-12-06 09:59:50 +0400

    adjustments

--- visualizations/correlation_table.py
+++ visualizations/correlation_table.py
@@ -21,7 +21,6 @@ final = perGameAdv.merge(fa, left_on = 'fix', right_on = 'name', how = 'inner')
 final.drop(final.columns[[27]], axis = 1, inplace = True)
 final.head()
 final2 = final.copy(deep=True)
-print(final.columns)
 final.drop(final.columns[[0, 26,25, 46]], axis = 1, inplace = True)
 
 # Check correlation between features
@@ -52,71 +51,60 @@ fig1 = sns.heatmap(corr,
 fig1.figure.savefig("correlation_table.png")
 
 ########################################################################################################################
-print(final2.columns)
+
 #Selecting candidates
-final2.drop(final2.columns[[0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21,
-                         25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                         41, 42]], axis = 1, inplace = True)
-# print(final2.columns)
-#
-
-#
-# Index(['MP', 'FG', 'FGA', 'FT', 'DRB', 'STL', 'BLK', 'TOV', 'PS.G', 'PER',
-#        'DWS', 'OBPM', 'DBPM', 'BPM', 'VORP', 'total-contract', 'average'],
-#       dtype='object')
- #piti mnan sranq
+final2.drop(final2.columns.difference(["GS", "MP","FG", "FGA", "X2P", "X2PA", "FT", "TOV","PTS.G",
+                                       "VORP","X3P","X3PA","DRB","TRB","OWS","DWS","WS"]), axis = 1, inplace = True)
 
 
+#Check correlation between features
+sns.set(font_scale=2)
+colormap = plt.cm.magma
+plt.figure(figsize=(30,30))
 
-# #Check correlation between features
-# sns.set(font_scale=2)
-# colormap = plt.cm.magma
-# plt.figure(figsize=(30,30))
-#
-# plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
-#
-# corr = final2.astype(float).corr().round(2)
-#
-# mask = np.zeros_like(corr, dtype=bool)
-#
-# # mask upper diagonal of heatmap
-# mask[np.triu_indices_from(mask)] = True
-#
-# fig2 = sns.heatmap(corr,mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
-# fig2.figure.savefig("more_precise_correlation_table.png")
-#
+plt.title('Pearson Correlation of NBA Salary Features', y=1.05, size=50)
+
+corr = final2.astype(float).corr().round(2)
+
+mask = np.zeros_like(corr, dtype=bool)
+
+# mask upper diagonal of heatmap
+mask[np.triu_indices_from(mask)] = True
+
+fig2 = sns.heatmap(corr,mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
+fig2.figure.savefig("more_precise_correlation_table.png")
+
 # ########################################################################################################################
+
+#Selecting candidates
+final2.drop(final2.columns.difference(["WS","OWS","DWS","DRB","X2P","TRB"] ), axis = 1, inplace = True)
+# Check correlation between features
+sns.set(font_scale=3)
+colormap = plt.cm.magma
+plt.figure(figsize=(30,30))
+
+plt.title('Correlation of Selected Variables', y=1.05, size=50)
+
+corr = final2.astype(float).corr().round(2)
+mask = np.zeros_like(corr, dtype=bool)
+
+# mask upper diagonal of heatmap
+mask[np.triu_indices_from(mask)] = True
+
+fig3 = sns.heatmap(corr, mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap,
+            linecolor='white', annot=True, annot_kws={"size": 50})
+fig3.figure.savefig("final_correlation_table.png")
+
+
+# In baseball, value over replacement player (or VORP) is a statistic popularized by Keith Woolner that
+# demonstrates how much a hitter or pitcher contributes to their team in comparison to a replacement-level
+# player who is an average fielder at that position and a below average hitter.
+
+
+# # Average points per game
 #
-# #Selecting candidates
-# final2.drop(final2.columns[[0, 1, 2, 3, 5, 6, 9, 10, 11, 12, 13, 15 ]], axis = 1, inplace = True)
+# # Average turnovers per game
 #
-# # Check correlation between features
-# sns.set(font_scale=3)
-# colormap = plt.cm.magma
-# plt.figure(figsize=(30,30))
+# # Defensive rebounds per game
 #
-# plt.title('Correlation of Selected Variables', y=1.05, size=50)
-#
-# corr = final2.astype(float).corr().round(2)
-# mask = np.zeros_like(corr, dtype=bool)
-#
-# # mask upper diagonal of heatmap
-# mask[np.triu_indices_from(mask)] = True
-#
-# fig3 = sns.heatmap(corr, mask = mask, linewidths=0.1,vmax=1.0, square=True, cmap=colormap,
-#             linecolor='white', annot=True, annot_kws={"size": 50})
-# fig3.figure.savefig("final_correlation_table.png")
-#
-#
-# # In baseball, value over replacement player (or VORP) is a statistic popularized by Keith Woolner that
-# # demonstrates how much a hitter or pitcher contributes to their team in comparison to a replacement-level
-# # player who is an average fielder at that position and a below average hitter.
-#
-#
-# # # Average points per game
-# #
-# # # Average turnovers per game
-# #
-# # # Defensive rebounds per game
-# #
-# # # overpaid underpaid  todo!!!
+# # overpaid underpaid  todo!!!
